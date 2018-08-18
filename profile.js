976b62ea7a00fe73e6b4e1012907a69ed600dc05
okikokim808 <kikomanistheman15@gmail.com>
2018-08-17 17:28:55 -0700

    just needs smooth scrolls and phone version

--- profile.js
+++ profile.js
@@ -1,15 +1,19 @@
 console.log("All good under the hood")
-var quote1 = "Francisco is extremely confident, hard working, and dependable. His great communication skills and professionalism makes him fun to work with. Overall he’s a great colleague to have!";
-var quote2 = "Francisco is extremely confident, hard working, and dependable. His great communication skills and professionalism makes him fun to work with. Overall he’s a great colleague to have!";
-var quote3 = "Francisco is extremely confident, hard working, and dependable. His great communication skills and professionalism makes him fun to work with. Overall he’s a great colleague to have!";
+var quote1 = "<p>Francisco is extremely confident, hard working, and dependable. His great communication skills and professionalism makes him fun to work with. Overall he’s a great colleague to have! <br> -Raj Mahal</p>";
+var quote2 = "<p>Francisco has hair <br> -Justin Castillo</p>";
+var quote3 = "<p>Francisco is extremely confident, hard working, and dependable. His great communication skills and professionalism makes him fun to work with. Overall he’s a great colleague to have! <br> -Raj Mahal</p>";
 
 var quotes = [quote1, quote2, quote3];
+var counter = 0;
 
-var p = $('.quotes');
-var index = 0;
-function showNextQuote(){
-    index++;
-    p.text(quotes[index % 3]);
-    p.fadeIn(500).delay(500).fadeOut(500, showNextQuote());
+function showQuote(){
+    $('.quotes').fadeTo(2000, 0, function(){
+        this.innerHTML=quotes[counter];
+        counter = ++counter % quotes.length;
+        $(this).fadeTo(2000, 1, showQuote);
+    });
+    // $('#quotes').fadeIn(2000);
+    // $('#quotes').delay(2000);
+    // $('#quotes').fadeOut(2000);
 }
-showNextQuote();
+showQuote();
